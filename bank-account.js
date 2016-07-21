2d580f59cfa088ffdee37101aac6164771abf392
Avinash <avinash.jairam@gmail.com>
2016-07-21 14:30:21 -0400

    Tested all functions. they work

--- bank-account.js
+++ bank-account.js
@@ -3,16 +3,24 @@ var account = {
 };
 
 function deposit(account, amount){
-	
+	account.balance += amount;	
 }
 
 
 function withdraw(account, amount){
-
+	account.balance -= amount; 
 
 }
 
 
 function getBalance(account){
-
+	return account.balance; 
 }
+
+deposit(account, 1000);
+
+console.log(getBalance(account));
+
+withdraw(account,121);
+
+console.log(getBalance(account));
